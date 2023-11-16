1095661ae536591a3338023915388301e5734e8b
Mareep <62206240+vveg26@users.noreply.github.com>
2023-11-16 22:56:30 +0800

    Update sshconfig.sh

--- BashScript/sshconfig.sh
+++ BashScript/sshconfig.sh
@@ -12,19 +12,26 @@ else
     ssh-keygen -t rsa
 fi
 
-    # 获取用户输入的端口号、用户名和IP地址
-    read -p "Enter the SSH port (default is 22): " port
-    read -p "Enter the username: " username
-    read -p "Enter the IP address: " ip_address
+# 获取用户输入的端口号、用户名和IP地址
+read -p "Enter the SSH port (default is 22): " port
+read -p "Enter the username: " username
+read -p "Enter the IP address: " ip_address
 
-    # 如果用户未输入端口号，则使用默认端口号 22
-    port=${port:-22}
+# 如果用户未输入端口号，则使用默认端口号 22
+port=${port:-22}
 
-    # 构建 ssh-copy-id 命令
-    ssh_copy_id_command="ssh-copy-id -p $port $username@$ip_address"
+# 检查并删除已知主机文件中的旧密钥
+known_hosts_file="$HOME/.ssh/known_hosts"
+if grep -q "$ip_address" "$known_hosts_file"; then
+    echo "Old key found for $ip_address. Removing it from $known_hosts_file."
+    ssh-keygen -f "$known_hosts_file" -R "$ip_address"
+fi
 
-    # 执行命令
-    echo "Running: $ssh_copy_id_command"
-    $ssh_copy_id_command
+# 构建 ssh-copy-id 命令
+ssh_copy_id_command="ssh-copy-id -p $port $username@$ip_address"
 
-    echo "You can now connect to your VPS using: ssh -p $port $username@$ip_address"
+# 执行命令
+echo "Running: $ssh_copy_id_command"
+$ssh_copy_id_command
+
+echo "You can now connect to your VPS using: ssh -p $port $username@$ip_address"
